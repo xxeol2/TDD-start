69a3d63a400ca9e0652e5d2414875ad83dcfaf59
xxeol2 <jhj01177@naver.com>
2022-11-03 13:58:06 +0900

    테스트코드 리팩토링

--- src/test/java/chap02/PasswordStrengthMeterTest.java
+++ src/test/java/chap02/PasswordStrengthMeterTest.java
@@ -5,28 +5,27 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class PasswordStrengthMeterTest {
+	private PasswordStrengthMeter meter = new PasswordStrengthMeter();
+
+	private void assertStrength(String password, PasswordStrength expStr) {
+		PasswordStrength result = meter.meter(password);
+		assertEquals(expStr, result);
+	}
+
 	@Test
 	void meetsAllCriteria_Then_Strong() {
-		PasswordStrengthMeter meter = new PasswordStrengthMeter();
-		PasswordStrength result = meter.meter("ab12!@AB");
-		assertEquals(PasswordStrength.STRONG, result);
-		PasswordStrength result2 = meter.meter("abc!Add");
-		assertEquals(PasswordStrength.STRONG, result2);
+		assertStrength("ab12!@AB", PasswordStrength.STRONG);
+		assertStrength("abc1!Add", PasswordStrength.STRONG);
 	}
 
 	@Test
 	void meetsOtherCriteria_except_for_Length_Then_Normal() {
-		PasswordStrengthMeter meter = new PasswordStrengthMeter();
-		PasswordStrength result = meter.meter("ab12!@A");
-		assertEquals(PasswordStrength.NORMAL, result);
-		PasswordStrength result2 = meter.meter("Ab12!c");
-		assertEquals(PasswordStrength.NORMAL, result2);
+		assertStrength("ab12!@A", PasswordStrength.NORMAL);
+		assertStrength("Ab12!c", PasswordStrength.NORMAL);
 	}
 
 	@Test
 	void meetsOtherCriteria_except_for_number_Then_Normal() {
-		PasswordStrengthMeter meter = new PasswordStrengthMeter();
-		PasswordStrength result = meter.meter("ab!@ABqwer");
-		assertEquals(PasswordStrength.NORMAL, result);
+		assertStrength("ab!@ABqwer", PasswordStrength.NORMAL);
 	}
 }
