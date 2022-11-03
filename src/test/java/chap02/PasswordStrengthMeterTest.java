390691a68b559b8a70c1a7aa77bfa854843e1851
xxeol2 <jhj01177@naver.com>
2022-11-03 13:44:02 +0900

    [test2] 길이가 8미만이고 나머지 조건은 충족하는 암호에 대한 테스트 추가

--- src/test/java/chap02/PasswordStrengthMeterTest.java
+++ src/test/java/chap02/PasswordStrengthMeterTest.java
@@ -13,4 +13,11 @@ public class PasswordStrengthMeterTest {
 		PasswordStrength result2 = meter.meter("abc!Add");
 		assertEquals(PasswordStrength.STRONG, result2);
 	}
+
+	@Test
+	void meetsOtherCriteria_except_for_Length_Then_Normal() {
+		PasswordStrengthMeter meter = new PasswordStrengthMeter();
+		PasswordStrength result = meter.meter("ab12!@A");
+		assertEquals(PasswordStrength.NORMAL, result);
+	}
 }
