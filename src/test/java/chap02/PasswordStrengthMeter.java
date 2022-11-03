5fa829e70d2d751c938a60cad0fce46765f5b6ed
xxeol2 <jhj01177@naver.com>
2022-11-03 13:45:11 +0900

    [test2] 테스트를 통과시키기 위한 코드 추가

--- src/test/java/chap02/PasswordStrengthMeter.java
+++ src/test/java/chap02/PasswordStrengthMeter.java
@@ -2,6 +2,9 @@ package chap02;
 
 public class PasswordStrengthMeter {
 	public PasswordStrength meter(String s) {
+		if (s.length() < 8) {
+			return PasswordStrength.NORMAL;
+		}
 		return PasswordStrength.STRONG;
 	}
 }
