38f71aa3f01050dd85787bf69b36ae27c84a6d21
xxeol2 <jhj01177@naver.com>
2022-11-03 12:48:55 +0900

    CalculatorTest 작성

---
+++ src/test/java/chap02/CalculatorTest.java
@@ -0,0 +1,14 @@
+package chap02;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+
+	@Test
+	void plus() {
+		int result = Calculator.plus(1, 2);
+		assertEquals(3,result);
+	}
+}
