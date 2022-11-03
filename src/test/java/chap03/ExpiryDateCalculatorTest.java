ad712fe2e14d190402e2d065358c231791e9f4b1
xxeol2 <jhj01177@naver.com>
2022-11-03 16:17:12 +0900

    예를 추가하면서 구현을 일반화

--- src/test/java/chap03/ExpiryDateCalculatorTest.java
+++ src/test/java/chap03/ExpiryDateCalculatorTest.java
@@ -17,5 +17,13 @@ public class ExpiryDateCalculatorTest {
 		LocalDate expiryDate = cal.calculateExpiryDate(billingDate, payAmount);
 
 		assertEquals(LocalDate.of(2019, 4, 1), expiryDate);
+
+		LocalDate billingDate2 = LocalDate.of(2019, 5, 5);
+		int payAmount2 = 10_000;
+
+		ExpiryDateCalculator cal2 = new ExpiryDateCalculator();
+		LocalDate expiryDate2 = cal2.calculateExpiryDate(billingDate2, payAmount2);
+
+		assertEquals(LocalDate.of(2019, 6, 5), expiryDate2);
 	}
 }
