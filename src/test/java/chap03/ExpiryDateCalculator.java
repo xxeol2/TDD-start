ce06feee13c1d82f52989c8c557ab10ba0525be2
xxeol2 <jhj01177@naver.com>
2022-11-03 22:13:28 +0900

    리팩토링: 코드 정리

--- src/test/java/chap03/ExpiryDateCalculator.java
+++ src/test/java/chap03/ExpiryDateCalculator.java
@@ -7,14 +7,32 @@ public class ExpiryDateCalculator {
 	public LocalDate calculateExpiryDate(PayData payData) {
 		int addedMonths = payData.getPayAmount() / 10_000;
 		if (payData.getFirstBillingDate() != null) {
-			LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths);
-			if (payData.getFirstBillingDate().getDayOfMonth() != candidateExp.getDayOfMonth()) {
-				if (YearMonth.from(candidateExp).lengthOfMonth() < payData.getFirstBillingDate().getDayOfMonth()) {
-					return candidateExp.withDayOfMonth(YearMonth.from(candidateExp).lengthOfMonth());
-				}
-				return candidateExp.withDayOfMonth(payData.getFirstBillingDate().getDayOfMonth());
-			}
+			return expiryDateUsingFirstBillingDate(payData, addedMonths);
+		} else {
+			return payData.getBillingDate().plusMonths(addedMonths);
 		}
-		return payData.getBillingDate().plusMonths(addedMonths);
+	}
+
+	public LocalDate expiryDateUsingFirstBillingDate(PayData payData, int addedMonths) {
+		LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths);
+		if (!iSameDayOfMonth(payData.getFirstBillingDate(), candidateExp)) {
+			final int dayLenOfCandiMon = lastDayOFMonth(candidateExp);
+			final int dayOfFirstBilling = payData.getFirstBillingDate().getDayOfMonth();
+			if (dayLenOfCandiMon < dayOfFirstBilling) {
+				return candidateExp.withDayOfMonth(dayLenOfCandiMon);
+			}
+			return candidateExp.withDayOfMonth(dayOfFirstBilling);
+		} else {
+			return candidateExp;
+		}
+	}
+
+	public boolean iSameDayOfMonth(LocalDate date1, LocalDate date2) {
+		return date1.getDayOfMonth() == date2.getDayOfMonth();
+	}
+
+	public int lastDayOFMonth(LocalDate date) {
+		return YearMonth.from(date).lengthOfMonth();
 	}
 }
+
